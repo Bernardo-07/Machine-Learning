8ac604b4d3b4cecc2bc3aaae8a43426d8b1961cb
Bernardo <bernardoduarte271@gmail.com>
2024-08-01 16:58:14 -0300

    final lab 1

--- IBM/lab1.py
+++ IBM/lab1.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import r2_score
 import csv
 
 '''data = 'C:\\Users\Bernardo Duarte\Documents\Machine-Learning\IBM\FuelConsumptionCo2.csv'
@@ -62,3 +64,40 @@ plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS, color='blue')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
+
+# Divisão do conjunto de dados em treinamento e teste
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+# Visualização dos dados de treinamento
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+# Criação do modelo de regressão linear
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+
+# Coeficientes e intercepto do modelo
+print('Coefficients: ', regr.coef_)
+print('Intercept: ', regr.intercept_)
+
+# Visualização do modelo ajustado
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color='blue')
+plt.plot(train_x, regr.coef_[0][0] * train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+# Avaliação do modelo
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y, test_y_))
